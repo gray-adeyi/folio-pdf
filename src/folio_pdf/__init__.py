a51ba83df5bc4ce54c67a80a93422798d88f2b03
Gbenga Adeyi <adeyigbenga005@gmail.com>
2026-04-25 19:48:31 +0100

    Update ctypes import with alias

--- src/folio_pdf/__init__.py
+++ src/folio_pdf/__init__.py
@@ -1,13 +1,12 @@
-import ctypes.wintypes
 from folio_pdf.document import Document
 from io import BytesIO
 from .core import lib
-import ctypes
+import ctypes as ct
 from pathlib import Path
 
 
-lib.folio_html_to_pdf.argtypes = [ctypes.c_char_p, ctypes.c_char_p]
-lib.folio_html_to_pdf.restype = ctypes.c_int32
+lib.folio_html_to_pdf.argtypes = [ct.c_char_p, ct.c_char_p]
+lib.folio_html_to_pdf.restype = ct.c_int32
 
 
 def html_to_pdf(html: str, destination: str | Path):
@@ -17,51 +16,50 @@ def html_to_pdf(html: str, destination: str | Path):
     if not _destination.endswith("pdf"):
         _destination += ".pdf"
     return lib.folio_html_to_pdf(
-        ctypes.c_char_p(html.encode()), ctypes.c_char_p(_destination.encode())
+        ct.c_char_p(html.encode()), ct.c_char_p(_destination.encode())
     )
 
 
-lib.folio_html_to_buffer.argtypes = [ctypes.c_char_p, ctypes.c_double, ctypes.c_double]
-lib.folio_html_to_buffer.restype = ctypes.c_int64
+lib.folio_html_to_buffer.argtypes = [ct.c_char_p, ct.c_double, ct.c_double]
+lib.folio_html_to_buffer.restype = ct.c_int64
 
-lib.folio_buffer_data.restype = ctypes.c_void_p
+lib.folio_buffer_data.restype = ct.c_void_p
 
 
 def html_to_buffer(html: str, page_width: float, page_height: float) -> BytesIO:
     buf = lib.folio_html_to_buffer(
-        ctypes.c_char_p(html.encode()),
-        ctypes.c_double(page_width),
-        ctypes.c_double(page_height),
+        ct.c_char_p(html.encode()),
+        ct.c_double(page_width),
+        ct.c_double(page_height),
     )
     size = lib.folio_buffer_len(buf)
     ptr = lib.folio_buffer_data(buf)
-    data = ctypes.string_at(ptr, size)
+    data = ct.string_at(ptr, size)
     lib.folio_buffer_free(buf)
     return BytesIO(data)
 
 
 def html_convert(html: str, page_width: float, page_height: float) -> Document:
-    doc_ptr = lib.folio_html_convert(
-        ctypes.c_char_p(html.encode()),
-        ctypes.c_double(page_width),
-        ctypes.c_double(page_height),
+    doc_handle = lib.folio_html_convert(
+        ct.c_char_p(html.encode()),
+        ct.c_double(page_width),
+        ct.c_double(page_height),
     )
-    return Document._new_from_ptr(doc_ptr)
+    return Document._new_from_handle(doc_handle)
 
 
 lib.folio_html_parse_css_length.argtypes = [
-    ctypes.c_char_p,
-    ctypes.c_double,
-    ctypes.c_double,
+    ct.c_char_p,
+    ct.c_double,
+    ct.c_double,
 ]
-lib.folio_html_parse_css_length.restype = ctypes.c_double
+lib.folio_html_parse_css_length.restype = ct.c_double
 
 
 def html_parse_css_length(s: str, font_size: float, relative_to: float) -> float:
-    result: ctypes.c_double = lib.folio_html_parse_css_length(
-        ctypes.c_char_p(s.encode()),
-        ctypes.c_double(font_size),
-        ctypes.c_double(relative_to),
+    result: ct.c_double = lib.folio_html_parse_css_length(
+        ct.c_char_p(s.encode()),
+        ct.c_double(font_size),
+        ct.c_double(relative_to),
     )
-    return result.value
-
+    return ct.c_double(result).value

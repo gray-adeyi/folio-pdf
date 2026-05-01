7cbfa4b115f73ab0eb43834e5ddc9cf211671567
Gbenga Adeyi <adeyigbenga005@gmail.com>
2026-05-01 14:20:53 +0100

    Move context manager functionality to AbstractFolioObject

--- src/folio_pdf/object.py
+++ src/folio_pdf/object.py
@@ -3,8 +3,25 @@ import ctypes as ct
 
 
 class AbstractFolioObject:
+    _requires_close: bool
+
     @property
     @abstractmethod
     def handle(self) -> ct.c_uint64:
         """Returns the object handle"""
         ...
+
+    def close(self):
+        err_msg = (
+            "close method has not been implemented for folio object"
+            if self._requires_close
+            else "folio object does not require close"
+        )
+        raise NotImplementedError(err_msg)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self):
+        if self._requires_close:
+            self.close()

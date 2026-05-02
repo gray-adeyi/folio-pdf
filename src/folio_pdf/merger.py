cedfa0eda04e79038db21840f1ef582286669dfc
Gbenga Adeyi <adeyigbenga005@gmail.com>
2026-05-02 19:08:06 +0100

    Implement merger

---
+++ src/folio_pdf/merger.py
@@ -0,0 +1,17 @@
+from folio_pdf.core import lib
+from folio_pdf.object import AbstractFolioObject
+import ctypes as ct
+
+
+class PDFMerger(AbstractFolioObject):
+    _requires_close = True
+
+    def __init__(self):
+        self._merger_handle = -1
+
+    @property
+    def handle(self) -> ct.c_uint64:
+        return ct.c_uint64(self._merger_handle)
+
+    def close(self):
+        lib.folio_merge_free(self.handle)

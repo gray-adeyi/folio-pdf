a159c6e750d90aa06755c901b6c0a8dcc371ed39
Gbenga Adeyi <adeyigbenga005@gmail.com>
2026-05-02 19:06:17 +0100

    Implement TableRow object

---
+++ src/folio_pdf/table_row.py
@@ -0,0 +1,19 @@
+from folio_pdf.object import AbstractFolioObject
+import ctypes as ct
+
+
+class TableRow(AbstractFolioObject):
+    _requires_close = True
+
+    def __init__(self):
+        self._row_handle = -1
+
+    @property
+    def handle(self) -> ct.c_uint64:
+        return ct.c_uint64(self._row_handle)
+
+    @classmethod
+    def _new_from_handle(cls, row_handle: int):
+        obj = cls.__new__(cls)
+        cls._row_handle = row_handle
+        return obj

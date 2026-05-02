0e4f74643b1e719215deb1f27e048b20a3482177
Gbenga Adeyi <adeyigbenga005@gmail.com>
2026-05-02 19:08:24 +0100

    Implement link

---
+++ src/folio_pdf/link.py
@@ -0,0 +1,29 @@
+from folio_pdf.object import AbstractFolioObject
+from folio_pdf.core import lib
+import ctypes as ct
+
+
+class Link(AbstractFolioObject):
+    _requires_close = True
+
+    def __init__(self):
+        self._link_handle = -1
+
+    @property
+    def handle(self) -> ct.c_uint64:
+        return ct.c_uint64(self._link_handle)
+
+    def close(self):
+        lib.folio_link_free(self.handle)
+
+    @classmethod
+    def new_embedded(cls): ...
+
+    @classmethod
+    def new_internal(cls): ...
+
+    def set_color(self): ...
+
+    def set_underline(self): ...
+
+    def set_align(self): ...

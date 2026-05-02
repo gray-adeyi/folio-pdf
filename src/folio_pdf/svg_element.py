e13c333b4866e6515afb551eba451ee92fe64347
Gbenga Adeyi <adeyigbenga005@gmail.com>
2026-05-02 19:06:56 +0100

    Implement svg element

---
+++ src/folio_pdf/svg_element.py
@@ -0,0 +1,23 @@
+from folio_pdf.core import lib
+from folio_pdf.object import AbstractFolioObject
+import ctypes as ct
+
+
+class SVGElement(AbstractFolioObject):
+    _requires_close = True
+
+    def __init__(self):
+        self._svg_element_handle = -1
+
+    @property
+    def handle(self) -> ct.c_uint64:
+        return ct.c_uint64(self._svg_element_handle)
+
+    def close(self):
+        lib.folio_svg_element_free(self.handle)
+
+    def set_size(self): ...
+
+    def set_align(self): ...
+
+    def set_alt_text(self): ...

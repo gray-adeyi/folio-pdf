4a9f5fe83a15c0f656194fd6a93d31db1ce2836b
Gbenga Adeyi <adeyigbenga005@gmail.com>
2026-05-02 19:09:16 +0100

    Implement barcode

---
+++ src/folio_pdf/barcode.py
@@ -0,0 +1,32 @@
+from folio_pdf.core import lib
+from folio_pdf.object import AbstractFolioObject
+import ctypes as ct
+
+
+class Barcode(AbstractFolioObject):
+    _requires_close = True
+
+    def __init__(self):
+        self._barcode_handle = -1
+
+    @property
+    def handle(self) -> ct.c_uint64:
+        return ct.c_uint64(self._barcode_handle)
+
+    def close(self):
+        lib.folio_barcode_free(self.handle)
+
+    @classmethod
+    def new_qr_ecc(cls): ...
+
+    @classmethod
+    def new_code128(cls): ...
+
+    @classmethod
+    def new_ean13(cls): ...
+
+    @property
+    def width(self): ...
+
+    @property
+    def height(self): ...

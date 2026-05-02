3a12a113ad17c1db611b37b2beb7be3792e84f8e
Gbenga Adeyi <adeyigbenga005@gmail.com>
2026-05-02 19:10:21 +0100

    Implement line separator

---
+++ src/folio_pdf/line_separator.py
@@ -0,0 +1,13 @@
+from folio_pdf.object import AbstractFolioObject
+import ctypes as ct
+
+
+class LineSeparator(AbstractFolioObject):
+    _requires_close = False
+
+    def __init__(self):
+        self._line_separator_handle = -1
+
+    @property
+    def handle(self) -> ct.c_uint64:
+        return ct.c_uint64(self._line_separator_handle)

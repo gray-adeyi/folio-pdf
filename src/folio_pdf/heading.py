1a87bb6767f652fc3c8717a2b564092641e40c16
Gbenga Adeyi <adeyigbenga005@gmail.com>
2026-05-04 10:38:04 +0100

    Add new method to list

--- src/folio_pdf/heading.py
+++ src/folio_pdf/heading.py
@@ -76,4 +76,6 @@ class Heading(AbstractFolioObject):
     def set_align(self, align: Alignments):
         return lib.folio_heading_set_align(self.handle, ct.c_int32(align.value))
 
-    def set_runs(self, run_list: RunList): ...
+    @_with_error_handling(HeadingException)
+    def set_runs(self, run_list: RunList):
+        return lib.folio_heading_set_runs(self.handle, run_list.handle)

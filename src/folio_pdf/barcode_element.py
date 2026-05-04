a0385b3a09ad7b191e5e110e2f84f92c2bb006df
Gbenga Adeyi <adeyigbenga005@gmail.com>
2026-05-04 10:36:24 +0100

    Implement barcode element

--- src/folio_pdf/barcode_element.py
+++ src/folio_pdf/barcode_element.py
@@ -3,7 +3,12 @@ Copyright 2026 Gbenga Adeyi and Folio PDF Authors
 SPDX-License-Identifier: Apache-2.0
 """
 
-from folio_pdf.core import lib
+from folio_pdf.exceptions import BarcodeElementException
+
+from folio_pdf.enums import Alignments
+from folio_pdf.barcode import Barcode
+
+from folio_pdf.core import lib, _with_error_handling
 from folio_pdf.object import AbstractFolioObject
 import ctypes as ct
 
@@ -11,8 +16,10 @@ import ctypes as ct
 class BarcodeElement(AbstractFolioObject):
     _requires_close = True
 
-    def __init__(self):
-        self._barcode_handle = -1
+    def __init__(self, bc: Barcode, width: float):
+        self._barcode_handle = lib.folio_barcode_element_new(
+            bc.handle, ct.c_double(width)
+        )
 
     @property
     def handle(self) -> ct.c_uint64:
@@ -21,8 +28,16 @@ class BarcodeElement(AbstractFolioObject):
     def close(self):
         lib.folio_barcode_element_free(self.handle)
 
-    def set_height(self): ...
+    @_with_error_handling(BarcodeElementException)
+    def set_height(self, height: float):
+        return lib.folio_barcode_element_set_height(self.handle, ct.c_double(height))
 
-    def set_align(self): ...
+    @_with_error_handling(BarcodeElementException)
+    def set_align(self, align: Alignments):
+        return lib.folio_barcode_element_set_align(self.handle, ct.c_int32(align.value))
 
-    def set_alt_text(self): ...
+    @_with_error_handling(BarcodeElementException)
+    def set_alt_text(self, text: str):
+        return lib.folio_barcode_element_set_alt_text(
+            self.handle, ct.c_char_p(text.encode())
+        )

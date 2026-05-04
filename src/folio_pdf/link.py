1a36059de5488861f523bd6e2938a841e7b8d57e
Gbenga Adeyi <adeyigbenga005@gmail.com>
2026-05-04 09:46:31 +0100

    Implement methods for link element

--- src/folio_pdf/link.py
+++ src/folio_pdf/link.py
@@ -3,16 +3,27 @@ Copyright 2026 Gbenga Adeyi and Folio PDF Authors
 SPDX-License-Identifier: Apache-2.0
 """
 
+from folio_pdf.exceptions import LinkException
+
+from folio_pdf.enums import Alignments
+
+from folio_pdf.font import Font
+
 from folio_pdf.object import AbstractFolioObject
-from folio_pdf.core import lib
+from folio_pdf.core import lib, _with_error_handling
 import ctypes as ct
 
 
 class Link(AbstractFolioObject):
     _requires_close = True
 
-    def __init__(self):
-        self._link_handle = -1
+    def __init__(self, text: str, uri: str, font: Font, font_size: float):
+        self._link_handle = lib.folio_link_new(
+            ct.c_char_p(text.encode()),
+            ct.c_char_p(uri.encode()),
+            font.handle,
+            ct.c_double(font_size),
+        )
 
     @property
     def handle(self) -> ct.c_uint64:
@@ -22,13 +33,36 @@ class Link(AbstractFolioObject):
         lib.folio_link_free(self.handle)
 
     @classmethod
-    def new_embedded(cls): ...
+    def new_embedded(cls, text: str, uri: str, font: Font, font_size: float):
+        obj = cls.__new__(cls)
+        obj._link_handle = lib.folio_link_new_embedded(
+            ct.c_char_p(text.encode()),
+            ct.c_char_p(uri.encode()),
+            font.handle,
+            ct.c_double(font_size),
+        )
+        return obj
 
     @classmethod
-    def new_internal(cls): ...
+    def new_internal(cls, text: str, dest_name: str, font: Font, font_size: float):
+        obj = cls.__new__(cls)
+        obj._link_handle = lib.folio_link_new_internal(
+            ct.c_char_p(text.encode()),
+            ct.c_char_p(dest_name.encode()),
+            font.handle,
+            ct.c_double(font_size),
+        )
 
-    def set_color(self): ...
+    @_with_error_handling(LinkException)
+    def set_color(self, r: float, g: float, b: float):
+        return lib.folio_link_set_color(
+            self.handle, ct.c_double(r), ct.c_double(g), ct.c_double(b)
+        )
 
-    def set_underline(self): ...
+    @_with_error_handling(LinkException)
+    def set_underline(self):
+        return lib.folio_link_set_underline(self.handle)
 
-    def set_align(self): ...
+    @_with_error_handling(LinkException)
+    def set_align(self, align: Alignments):
+        return lib.folio_link_set_align(self.handle, ct.c_int32(align.value))

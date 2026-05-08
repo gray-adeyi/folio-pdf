31a53a829bd1c5ec203ed0b946c0ed3dc34c7765
Gbenga Adeyi <adeyigbenga005@gmail.com>
2026-05-08 11:03:28 +0100

    Update tests

--- tests/test_folio_pdf/test_document.py
+++ tests/test_folio_pdf/test_document.py
@@ -1,21 +1,44 @@
-from folio_pdf.document import Document
+import os
+from pathlib import Path
 from unittest import TestCase
+from uuid import uuid4
+
+from folio_pdf import Font
+from folio_pdf.document import Document
+from folio_pdf.enums import StandardPDFFonts
 
 
 class DocumentTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.doc = Document.new_a4()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.doc.close()
+
     def test_can_create_doument_instance(self):
-        doc = Document(100, 100)
-        self.assertGreater(doc._doc_handle, 0)
+        with Document(100, 100) as doc:
+            self.assertGreater(doc.handle.value, 0)
 
     def test_can_create_a4_document_instance(self):
-        doc = Document.new_a4()
-        self.assertGreater(doc._doc_ptr, 0)
+        with Document.new_a4() as doc:
+            self.assertGreater(doc.handle.value, 0)
 
     def test_can_set_document_title(self):
-        doc = Document.new_a4()
-        doc.set_title("folio test document")
+        self.assertIsNone(self.doc.set_title("folio test document"))
 
     def test_can_save(self):
         doc = Document.new_a4()
         doc.set_title("folio test document")
-        doc.save("demo.pdf")
+        doc.set_watermark("Folio pdf test")
+        page = doc.add_page()
+        font = Font(StandardPDFFonts.HELVETICA_BOLD)
+        page.add_text("I love folio pdf", font, 14, 100, 100)
+        filename = f"{uuid4()}.pdf"
+        doc.save(filename)
+        filepath = Path(filename)
+        self.assertTrue(filepath.exists())  # saved pdf does exist
+        self.assertTrue(filepath.is_file())  # saved pdf is file
+        self.assertGreater(filepath.stat().st_size, 0)  # saved pdf is non-empty
+        os.remove(filepath)

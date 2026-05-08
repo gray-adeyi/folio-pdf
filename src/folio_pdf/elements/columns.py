eef0dc04ea9b0c2259d6c59e8e4d7a9088630b82
Gbenga Adeyi <adeyigbenga005@gmail.com>
2026-05-08 15:27:39 +0100

    Add methods spec

--- src/folio_pdf/elements/columns.py
+++ src/folio_pdf/elements/columns.py
@@ -11,12 +11,20 @@ from folio_pdf.core import AbstractFolioObject, lib
 class Column(AbstractFolioObject):
     _requires_close = True
 
-    def __init__(self):
-        self._column_handle = lib.folio_column_new()
+    def __init__(self, cols: int):
+        self._column_handle = lib.folio_columns_new(ct.c_int32(cols))
 
     @property
     def handle(self) -> ct.c_uint64:
         return ct.c_uint64(self._column_handle)
 
     def close(self):
-        lib.folio_grid_free(self.handle)
+        lib.folio_columns_free(self.handle)
+
+    def set_gap(self, gap: float): ...
+
+    def set_widths(self): ...
+
+    def set_balanced(self): ...
+
+    def add(self): ...

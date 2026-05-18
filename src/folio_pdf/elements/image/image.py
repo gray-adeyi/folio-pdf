d92ac105dc9ed4fbefa1df180c93b35c80f474f9
Gbenga Adeyi <adeyigbenga005@gmail.com>
2026-05-18 13:50:44 +0100

    Document image

--- src/folio_pdf/elements/image/image.py
+++ src/folio_pdf/elements/image/image.py
@@ -9,22 +9,56 @@ from pathlib import Path
 from folio_pdf.core import AbstractFolioObject, lib
 from folio_pdf.exceptions import ImageException
 
+lib.folio_image_load_jpeg.argtypes = [ct.c_char_p]
+lib.folio_image_load_jpeg.restype = ct.c_uint64
+
+lib.folio_image_load_png.argtypes = [ct.c_char_p]
+lib.folio_image_load_png.restype = ct.c_uint64
+
+lib.folio_image_load_tiff.argtypes = [ct.c_char_p]
+lib.folio_image_load_tiff.restype = ct.c_uint64
+
+lib.folio_image_parse_jpeg.argtypes = [ct.c_void_p, ct.c_int32]
+lib.folio_image_parse_jpeg.restype = ct.c_uint64
+
+lib.folio_image_parse_png.argtypes = [ct.c_void_p, ct.c_int32]
+lib.folio_image_parse_png.restype = ct.c_uint64
+
+lib.folio_image_width.argtypes = [ct.c_uint64]
+lib.folio_image_width.restype = ct.c_int32
+
+lib.folio_image_height.argtypes = [ct.c_uint64]
+lib.folio_image_height.restype = ct.c_int32
+
+lib.folio_image_free.argtypes = [ct.c_uint64]
+lib.folio_image_free.restype = None
+
 
 class Image(AbstractFolioObject):
+    """
+    Represents an image that can be added to a `ImageElement`.
+    """
+
     _requires_close = True
 
     def __init__(self):
         self.__handle = -1
 
-    @property
-    def _handle(self) -> ct.c_uint64:
-        return ct.c_uint64(self.__handle)
-
-    def close(self):
-        lib.folio_image_free(self._handle)
-
     @classmethod
-    def load(cls, path: str | Path):
+    def load(cls, path: str | Path) -> "Image":
+        """
+        Loads an image from the given file path.
+
+        This uses the extension in the path to determin how to load
+        the iamge and only currently supports, jpeg, png and tiff.
+
+        Args:
+            path: absolute path to the image file
+
+        Returns:
+            a new `Image` which can then be used to create and `ImageElement`,
+            which can then be added to a document or div
+        """
         _path = path
         if isinstance(_path, str):
             _path = Path(_path)
@@ -38,7 +72,17 @@ class Image(AbstractFolioObject):
         raise ImageException(f"loading image with the extension {ext} is not supported")
 
     @classmethod
-    def load_jpeg(cls, path: str | Path):
+    def load_jpeg(cls, path: str | Path) -> "Image":
+        """
+        Loads a JPEG image from the given file path.
+
+        Args:
+            path: absolute path to the `.jpg` / `.jpeg` file
+
+        Returns:
+            a new `Image` which can then be used to create and `ImageElement`,
+            which can then be added to a document or div
+        """
         _path = path
         if isinstance(_path, Path):
             _path = _path.as_posix()
@@ -47,7 +91,17 @@ class Image(AbstractFolioObject):
         return obj
 
     @classmethod
-    def load_png(cls, path: str | Path):
+    def load_png(cls, path: str | Path) -> "Image":
+        """
+        Loads a PNG image from the given file path.
+
+        Args:
+            path: absolute path to the `.png` file
+
+        Returns:
+            a new `Image` which can then be used to create and `ImageElement`,
+            which can then be added to a document or div
+        """
         _path = path
         if isinstance(_path, Path):
             _path = _path.as_posix()
@@ -56,7 +110,17 @@ class Image(AbstractFolioObject):
         return obj
 
     @classmethod
-    def load_tiff(cls, path: str | Path):
+    def load_tiff(cls, path: str | Path) -> "Image":
+        """
+        Loads a TIFF image from the given file path.
+
+        Args:
+            path: absolute path to the `.tif` / `.tiff` file
+
+        Returns:
+            a new `Image` which can then be used to create and `ImageElement`,
+            which can then be added to a document or div
+        """
         _path = path
         if isinstance(_path, Path):
             _path = _path.as_posix()
@@ -65,7 +129,17 @@ class Image(AbstractFolioObject):
         return obj
 
     @classmethod
-    def parse_jpeg(cls, data: bytes):
+    def parse_jpeg(cls, data: bytes) -> "Image":
+        """
+        Parses a JPEG image from raw bytes.
+
+        Args:
+            data: the raw JPEG bytes
+
+        Returns:
+            a new `Image` which can then be used to create and `ImageElement`,
+            which can then be added to a document or div
+        """
         obj = cls.__new__(cls)
         obj.__handle = lib.folio_image_parse_jpeg(
             ct.c_char_p(data), ct.c_int32(len(data))
@@ -73,7 +147,17 @@ class Image(AbstractFolioObject):
         return obj
 
     @classmethod
-    def parse_png(cls, data: bytes):
+    def parse_png(cls, data: bytes) -> "Image":
+        """
+        Parses a PNG image from raw bytes.
+
+        Args:
+            data: the raw PNG bytes
+
+        Returns:
+            a new `Image` which can then be used to create and `ImageElement`,
+            which can then be added to a document or div
+        """
         obj = cls.__new__(cls)
         obj.__handle = lib.folio_image_parse_jpeg(
             ct.c_char_p(data), ct.c_int32(len(data))
@@ -81,9 +165,22 @@ class Image(AbstractFolioObject):
         return obj
 
     @property
-    def width(self):
+    def width(self) -> int:
+        """
+        Returns the natural pixel width of the source image.
+        """
         return lib.folio_image_width(self._handle)
 
     @property
-    def height(self):
+    def height(self) -> int:
+        """
+        Returns the natural pixel height of the source image.
+        """
         return lib.folio_image_height(self._handle)
+
+    def close(self):
+        lib.folio_image_free(self._handle)
+
+    @property
+    def _handle(self) -> ct.c_uint64:
+        return ct.c_uint64(self.__handle)

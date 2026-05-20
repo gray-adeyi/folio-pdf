6a2f3a9a5a236098b871951365b7655127c72b37
Gbenga Adeyi <adeyigbenga005@gmail.com>
2026-05-20 19:51:07 +0100

    Document svg

--- src/folio_pdf/elements/svg/svg.py
+++ src/folio_pdf/elements/svg/svg.py
@@ -7,22 +7,53 @@ import ctypes as ct
 
 from folio_pdf.core import AbstractFolioObject, lib
 
+lib.folio_svg_parse.argtypes = [ct.c_char_p]
+lib.folio_svg_parse.restype = ct.c_uint64
+
+lib.folio_svg_parse_bytes.argtypes = [ct.c_void_p, ct.c_int32]
+lib.folio_svg_parse_bytes.restype = ct.c_uint64
+
+lib.folio_svg_width.argtypes = [ct.c_uint64]
+lib.folio_svg_width.restype = ct.c_double
+
+lib.folio_svg_height.argtypes = [ct.c_uint64]
+lib.folio_svg_height.restype = ct.c_double
+
+lib.foli_svg_free.argtypes = [ct.c_uint64]
+lib.foli_svg_free.restype = None
+
 
 class SVG(AbstractFolioObject):
+    """
+    A representation of an SVG, that can be passed to an `SVGElement` which
+    can then be added to a `Document` or `Div`
+    """
+
     _requires_close = True
 
     def __init__(self, svg_xml: str):
+        """
+        Parses an SVG document from an XML string.
+
+        Args:
+            svg_xml: the SVG XML content as a string
+
+        Returns:
+            a new `SVG` instance
+        """
         self.__handle = lib.folio_svg_parse(ct.c_char_p(svg_xml.encode()))
 
-    @property
-    def _handle(self) -> ct.c_uint64:
-        return ct.c_uint64(self.__handle)
-
-    def close(self):
-        lib.folio_svg_free(self._handle)
-
     @classmethod
-    def parse_bytes(cls, data: bytes):
+    def parse_bytes(cls, data: bytes) -> "SVG":
+        """
+        Parses an SVG document from raw bytes and creates a document element.
+
+        Args:
+            data: the SVG content as a byte array
+
+        Returns:
+            a new `SVG` instance
+        """
         obj = cls.__new__(cls)
         obj._svg_handle = lib.folio_svg_parse_bytes(
             ct.c_char_p(data), ct.c_int32(len(data))
@@ -30,9 +61,28 @@ class SVG(AbstractFolioObject):
         return obj
 
     @property
-    def width(self):
+    def width(self) -> float:
+        """
+        Returns the intrinsic width declared in the SVG source, in points.
+
+        Returns:
+            the SVG's natural width
+        """
         return lib.folio_svg_width(self._handle)
 
     @property
-    def height(self):
+    def height(self) -> float:
+        """
+        Returns the intrinsic height declared in the SVG source, in points.
+
+        Returns:
+            the SVG's natural height
+        """
         return lib.folio_svg_height(self._handle)
+
+    def close(self):
+        lib.folio_svg_free(self._handle)
+
+    @property
+    def _handle(self) -> ct.c_uint64:
+        return ct.c_uint64(self.__handle)
